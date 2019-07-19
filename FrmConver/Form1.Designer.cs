8364ac6be17d9f9478fcdc7a27b903fdfd3b1904
duanhui <duanhui@intest.cn>
2019-07-19 14:00:23 +0800

    111

---
+++ FrmConver/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace FrmConver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPending = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtPending
+            // 
+            this.txtPending.Location = new System.Drawing.Point(16, 16);
+            this.txtPending.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPending.Multiline = true;
+            this.txtPending.Name = "txtPending";
+            this.txtPending.Size = new System.Drawing.Size(254, 330);
+            this.txtPending.TabIndex = 0;
+            this.txtPending.DoubleClick += new System.EventHandler(this.txtPending_DoubleClick);
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(332, 16);
+            this.txtResult.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtResult.Multiline = true;
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(254, 330);
+            this.txtResult.TabIndex = 1;
+            this.txtResult.DoubleClick += new System.EventHandler(this.txtResult_DoubleClick);
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(279, 151);
+            this.btnConvert.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(43, 58);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "提取";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 360);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtPending);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "提取";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPending;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Button btnConvert;
+    }
+}
+
---
+++ FrmConver/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace FrmConver
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(txtPending.Text))
+                {
+                    string[] strList = txtPending.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+                    List<string> results = new List<string>();
+
+                    foreach (var item in new List<string>(strList))
+                    {
+                        if (item.Length > 32)
+                        {
+                            results.AddRange(new List<string>(Regex.Split(item, "\\s+", RegexOptions.IgnoreCase)));
+                        }
+                        else
+                        {
+                            results.Add(item);
+                        }
+                    }
+                    for (int i = 0; i < results.Count; i++)
+                    {
+                        Match match = Regex.Match(results[i], @"\d([0-9]|x|X)+");
+                        //Match match = Regex.Match(results[i], @"((\d{14}$)|(\d{17}([0-9]|X|x)$))+");
+                        results[i] = GetMatch(match).Value;
+                        //= Regex.Replace(results[i], @"[^0-9]+", "");
+                    }
+
+                    List<String> listResults = new List<string>();
+
+                    foreach (var item in results)
+                    {
+                        if (!string.IsNullOrEmpty(item) && item.Length > 13)
+                            listResults.Add(item);
+                    }
+                    for (int i = 0; i < listResults.Count; i++)
+                    {
+                        if (listResults[i].Length < 18)
+                        {
+                            listResults[i] = listResults[i].Substring(0, 14);
+                        }
+                        else
+                        {
+                            listResults[i] = listResults[i].Substring(0, 18);
+                        }
+                    }
+                    txtResult.Text = string.Join("\r\n", listResults);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("出错了，请重试~");
+            }
+        }
+
+        public Match GetMatch(Match math)
+        {
+            if (!string.IsNullOrEmpty(math.Value))
+            {
+                return math.Value.Length >= 14 ? math : GetMatch(math.NextMatch());
+            }
+            return math;
+        }
+
+        private void txtResult_DoubleClick(object sender, EventArgs e)
+        {
+            txtResult.SelectAll();
+        }
+
+        private void txtPending_DoubleClick(object sender, EventArgs e)
+        {
+            txtPending.SelectAll();
+        }
+    }
+}
